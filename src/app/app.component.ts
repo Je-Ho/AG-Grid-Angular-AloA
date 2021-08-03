d715d651aae17849b481f3a8761da1909d58b247
Je-Ho <jenshofmann@outlook.de>
2021-08-03 12:28:53 +0200

    1 file modified added Data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -167,6 +167,69 @@ export class AppComponent {
       Verfügbar: '2021-08-20',
       Aktion: 'Kontakt erstellen',
       Status: 'Abgeschlossen'
+    },
+    {
+      Art: 'Positive Schadenabwicklung',
+      VSNR: '452835420',
+      Dringlichkeit: 'hoch',
+      Abschlussquote: 'hoch',
+      Verfügbar: '2021-08-04',
+      Aktion: 'Kontakt erstellen',
+      Status: 'Offen'
+    },
+    {
+      Art: 'Upselling PHV',
+      VSNR: '6294205',
+      Dringlichkeit: 'niedrig',
+      Abschlussquote: 'hoch',
+      Verfügbar: '2021-09-04',
+      Aktion: 'Kontakt erstellen',
+      Status: 'In Arbeit'
+    },
+    {
+      Art: 'Geburtstag 18',
+      VSNR: '23423098',
+      Dringlichkeit: 'niedrig',
+      Abschlussquote: 'hoch',
+      Verfügbar: '2021-10-04',
+      Aktion: 'Kontakt erstellen',
+      Status: 'Offen'
+    },
+    {
+      Art: 'Ablauf Riester Förderung',
+      VSNR: '2342356',
+      Dringlichkeit: 'mittel',
+      Abschlussquote: 'hoch',
+      Verfügbar: '2021-08-06',
+      Aktion: 'Kontakt erstellen',
+      Status: 'In Arbeit'
+    },
+    {
+      Art: 'Vertragsverlängerung',
+      VSNR: '75346',
+      Dringlichkeit: 'hoch',
+      Abschlussquote: 'hoch',
+      Verfügbar: '2021-08-12',
+      Aktion: 'Kontakt erstellen',
+      Status: 'In Arbeit'
+    },
+    {
+      Art: 'Hohe Abschlusswahrscheinlichkeit Zahnzusatz',
+      VSNR: '23466236',
+      Dringlichkeit: 'niedrig',
+      Abschlussquote: 'mittel',
+      Verfügbar: '2021-08-14',
+      Aktion: 'Kontakt erstellen',
+      Status: 'Offen'
+    },
+    {
+      Art: 'Umzug',
+      VSNR: '75464352',
+      Dringlichkeit: 'mittel',
+      Abschlussquote: 'mittel',
+      Verfügbar: '2021-08-20',
+      Aktion: 'Kontakt erstellen',
+      Status: 'Abgeschlossen'
     }
   ];
 
